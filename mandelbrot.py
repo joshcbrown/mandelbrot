c68d2855f3c9267467d2300e12bcaa22659285e6
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2022-02-21 10:55:03 +1100

    added choice of palletes, renamed examples

--- mandelbrot.py
+++ mandelbrot.py
@@ -10,35 +10,50 @@ import random
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--centre', 
+    parser.add_argument('-c', '--centre',
                         help="centre of the image on the plane. give in the format"
-                             "x y", )
+                             "x y")
+    parser.add_argument("-cs", "--centre-string")
     parser.add_argument('-z', '--zoom', type=float)
-    parser.add_argument('-r', '--resolution', type=str, choices=["high", "med", "low"], default="med")
+    parser.add_argument('-r', '--resolution', type=str,
+                        choices=["ultra", "high", "med", "low"], default="med")
     parser.add_argument('-a', '--aspect-ratio', type=str,
                         help="give in the format x:y", default="16:9")
     parser.add_argument('-i', '--max-iters', type=int, default=300)
     parser.add_argument('-b', '--bound', type=int, default=500)
     parser.add_argument('-s', '--save-config', action='store_true')
+    parser.add_argument('-p', '--pallete', default='warm')
 
     args = parser.parse_args()
+
+    asdf = json.load(open('configs.json'))
+    pallete = asdf['palletes'][args.pallete]
+    args.vals = pallete['vals']
+    args.colours = pallete['colours']
+
     if args.centre is None or args.zoom is None:
-        asdf = json.load(open('configs.json'))
-        image_index = random.randint(0, len(asdf['images']) - 1)
-        args.centre = asdf['images'][image_index]['centre']
-        args.zoom = asdf['images'][image_index]['zoom']
+        if args.centre_string is None:
+            image_index = random.randint(0, len(asdf['images'].values()) - 1)
+            args.centre = asdf['images'].values()[image_index]['centre']
+            args.zoom = asdf['images'].values()[image_index]['zoom']
+        else:
+            args.centre = asdf['images'][args.centre_string]['centre']
+            args.zoom = asdf['images'][args.centre_string]['zoom']
     else:
         args.centre = tuple([float(point) for point in args.centre.split()])
 
-    args.aspect_ratio = tuple([int(num) for num in args.aspect_ratio.split(':')])
+    args.aspect_ratio = tuple([int(num)
+                              for num in args.aspect_ratio.split(':')])
 
     resolution_dict = {
+        'ultra': 240,
         'high': 120,
         'med': 60,
         'low': 20
     }
-    args.resolution = tuple([num * resolution_dict[args.resolution] for num in args.aspect_ratio])
-    
+    args.resolution = tuple(
+        [num * resolution_dict[args.resolution] for num in args.aspect_ratio])
+
     if args.save_config:
         asdf = json.load(open('configs.json'))
         new_image = {'centre': args.centre, 'zoom': args.zoom}
@@ -48,6 +63,7 @@ def get_args():
 
     return args
 
+
 def iter_count(c: complex, args: argparse.ArgumentParser) -> int:
     z = c
     for i in range(args.max_iters):
@@ -56,18 +72,17 @@ def iter_count(c: complex, args: argparse.ArgumentParser) -> int:
             return i
     return args.max_iters
 
+
 def main():
     args = get_args()
+    # TODO: comment it up
+    cs = PchipInterpolator(args.vals, args.colours)
 
-    VALS = (0.05, 0.1, 0.3, 0.6, 0.9, 1)
-    COLOURS = ((0, 18, 25), (0, 18, 25), (10, 147, 150), (233, 216, 166), (174, 32, 18), (0, 18, 25))
-    cs = PchipInterpolator(VALS, COLOURS)
-
-    x_axis = np.linspace(args.centre[0] - args.aspect_ratio[0]/args.zoom, 
-                         args.centre[0] + args.aspect_ratio[0]/args.zoom, 
+    x_axis = np.linspace(args.centre[0] - args.aspect_ratio[0]/args.zoom,
+                         args.centre[0] + args.aspect_ratio[0]/args.zoom,
                          args.resolution[0])
-    y_axis = np.linspace(args.centre[1] + args.aspect_ratio[1]/args.zoom, 
-                         args.centre[1] - args.aspect_ratio[1]/args.zoom, 
+    y_axis = np.linspace(args.centre[1] + args.aspect_ratio[1]/args.zoom,
+                         args.centre[1] - args.aspect_ratio[1]/args.zoom,
                          args.resolution[1])
     image = Image.new(mode="RGB", size=args.resolution)
 
@@ -90,6 +105,7 @@ def main():
 
     curr_time = time.strftime('%H:%M:%S', time.localtime())
     image.save(f'{curr_time}.png')
+
 
 if __name__ == '__main__':
     main()
