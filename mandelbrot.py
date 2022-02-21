30c89b3725dcdcba38589d4ea5ceeff537254601
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2022-02-21 16:26:57 +1100

    added option to load and save data from an image

--- mandelbrot.py
+++ mandelbrot.py
@@ -21,26 +21,34 @@ def get_args():
                         help="give in the format x:y", default="16:9")
     parser.add_argument('-i', '--max-iters', type=int, default=300)
     parser.add_argument('-b', '--bound', type=int, default=500)
-    parser.add_argument('-s', '--save-config', action='store_true')
+    parser.add_argument('-sc', '--save-config', type=str,
+                        help='name of image you want to save')
+    parser.add_argument('-sd', '--save-data', action='store_true')
+    parser.add_argument('-ld', '--load-data')
     parser.add_argument('-p', '--pallete', default='warm')
 
     args = parser.parse_args()
 
-    asdf = json.load(open('configs.json'))
-    pallete = asdf['palletes'][args.pallete]
+    config = json.load(open('configs.json'))
+    pallete = config['palletes'][args.pallete]
     args.vals = pallete['vals']
     args.colours = pallete['colours']
 
     if args.centre is None or args.zoom is None:
         if args.centre_string is None:
-            image_index = random.randint(0, len(asdf['images'].values()) - 1)
-            args.centre = asdf['images'].values()[image_index]['centre']
-            args.zoom = asdf['images'].values()[image_index]['zoom']
+            image_index = random.randint(0, len(config['images'].values()) - 1)
+            args.centre = [*config['images'].values()][image_index]['centre']
+            args.zoom = [*config['images'].values()][image_index]['zoom']
+            args.centre_string = [*config['images'].keys()][image_index]
         else:
-            args.centre = asdf['images'][args.centre_string]['centre']
-            args.zoom = asdf['images'][args.centre_string]['zoom']
+            args.centre = config['images'][args.centre_string]['centre']
+            args.zoom = config['images'][args.centre_string]['zoom']
     else:
         args.centre = tuple([float(point) for point in args.centre.split()])
+
+    # purely for naming purposes of the image
+    if args.centre_string is None:
+        args.centre_string = "custom"
 
     args.aspect_ratio = tuple([int(num)
                               for num in args.aspect_ratio.split(':')])
@@ -54,12 +62,13 @@ def get_args():
     args.resolution = tuple(
         [num * resolution_dict[args.resolution] for num in args.aspect_ratio])
 
-    if args.save_config:
-        asdf = json.load(open('configs.json'))
+    if args.save_config is not None:
+        #TODO: check for duplicates before saving
         new_image = {'centre': args.centre, 'zoom': args.zoom}
-        asdf['images'].append(new_image)
-        with open('configs.json', 'w') as f:
-            json.dump(asdf, f)
+        config['images'][args.save_config] = new_image
+        json.dump(config, open('configs.json', 'w'))
+        print("saved image. exiting...")
+        exit(0)
 
     return args
 
@@ -72,6 +81,9 @@ def iter_count(c: complex, args: argparse.ArgumentParser) -> int:
             return i
     return args.max_iters
 
+def save_image(image, args):
+    curr_time = time.strftime('%H:%M:%S', time.localtime())
+    image.save(f'{args.pallete}-{args.centre_string}-{curr_time}.png')
 
 def main():
     args = get_args()
@@ -84,28 +96,44 @@ def main():
     y_axis = np.linspace(args.centre[1] + args.aspect_ratio[1]/args.zoom,
                          args.centre[1] - args.aspect_ratio[1]/args.zoom,
                          args.resolution[1])
-    image = Image.new(mode="RGB", size=args.resolution)
+    
 
+    if args.load_data is not None:
+        hue_array = np.load(args.load_data)
+        image = Image.new(mode="RGB", size=hue_array.shape)
+        for i, row in tqdm(enumerate(hue_array)):
+            for j, num in enumerate(row):
+                hue = cs(num)
+                image.putpixel((i, j), tuple([int(num) for num in hue]))
+        save_image(image, args)
+        return
+
+    image = Image.new(mode="RGB", size=args.resolution)
     iteration_counts = np.zeros(args.resolution).astype(int)
     for i, x in tqdm(enumerate(x_axis)):
         for j, y in enumerate(y_axis):
             iteration_counts[i][j] = iter_count(complex(x, y), args)
 
     num_iters_pp = np.zeros(iteration_counts.max() + 1).astype(int)
-    for row in iteration_counts:
+    for row in tqdm(iteration_counts):
         for count in row:
             num_iters_pp[count] += 1
 
     total = args.resolution[0] * args.resolution[1]
-    for i, row in enumerate(iteration_counts):
+
+    if args.save_data:
+        hue_array = np.zeros_like(iteration_counts).astype(float)
+    for i, row in tqdm(enumerate(iteration_counts)):
         for j, count in enumerate(row):
             hue = cs(num_iters_pp[:count + 1].sum() / total)
             hue = [int(i) for i in hue]
+            if args.save_data:
+                hue_array[i][j] = num_iters_pp[:count + 1].sum() / total
             image.putpixel((i, j), tuple(hue))
-
-    curr_time = time.strftime('%H:%M:%S', time.localtime())
-    image.save(f'{curr_time}.png')
-
+    
+    save_image(image, args)
+    if args.save_data:
+        np.save(f'data/{args.centre_string}', hue_array)
 
 if __name__ == '__main__':
     main()
